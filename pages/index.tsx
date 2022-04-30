7f51fa23a8ed927c9d5ad2cda7632884db7fba96
LE123123 <heart20021010@gmail.com>
2022-04-30 11:14:12 +0900

    #4.3 Test Drive part Tree

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Container = tw.div`
   py-20
   px-20
   flex   
-  
+  min-h-screen
 `;
 
 const Home: NextPage = () => {
@@ -56,8 +56,42 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-2xl">
+        <div className="mb-5 flex items-center justify-between">
+          <span>⬅</span>
+          <div className="space-x-3">
+            <span>⭐️4.9</span>
+            <span className="rounded-md p-2 shadow-xl">💖</span>
+          </div>
+        </div>
+        <div className="mb-5 h-72 bg-zinc-400" />
+        <div className="flex flex-col">
+          <span className="mb-1.5 text-xl font-medium">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex items-center justify-between">
+            <div>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="flex aspect-square w-8 items-center justify-center rounded-xl bg-blue-200 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="flex aspect-square w-8 items-center justify-center rounded-xl bg-blue-200 text-xl  text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-2xl font-medium">$450</span>
+            <button className="rounded-lg bg-blue-500 py-2 px-8 text-center text-xs text-white">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
