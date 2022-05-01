70a3610aac493c1140459555b3ff232879286f66
LE123123 <heart20021010@gmail.com>
2022-05-01 11:34:15 +0900

    #5.3 ~ 5 Home, Item Detail, Upload Item UI

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -42,13 +42,14 @@ export default function Enter() {
           </div>
         </div>
         <form className="mt-8 flex flex-col">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-2">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 className="w-full appearance-none rounded-md border border-gray-300
                             px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500
@@ -62,6 +63,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="w-full appearance-none rounded-r-md border border-gray-300
                              px-3 py-2 placeholder-gray-400 shadow-sm  focus:border-orange-500 focus:outline-none focus:ring-orange-500"
