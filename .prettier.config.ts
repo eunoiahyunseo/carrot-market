0da8dcbcf75d25f79d2ae9c5b1a936023df4b2f6
LE123123 <heart20021010@gmail.com>
2022-05-03 16:23:28 +0900

    #6.5 ~ 8 Push To PlaentScale, Prisma Client, API Routes

--- .prettier.config.ts
+++ .prettier.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   tabWidth: 4,
   semi: true,
-  printWidth: 80,
+  printWidth: 30,
   plugins: [require("prettier-plugin-tailwindcss")],
 };
---
+++ libs/client.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+
+export default new PrismaClient();
+// client.user.create({ data: { email: "Hi", name: "hi" } });
