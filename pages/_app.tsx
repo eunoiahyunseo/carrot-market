4e3eb5bc074bd7dd6837cb78918bb7ed6ee4c4af
LE123123 <heart20021010@gmail.com>
2022-05-14 01:00:48 +0900

    #10.3 ~ 4 construct useUser by SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import useSWR, { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto w-full max-w-xl">
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
+      }}
+    >
+      <div className="mx-auto w-full max-w-xl">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import useUser from "@libs/client/useUser";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  const user = useUser();
+  const { user, isLoading } = useUser();
   console.log(user);
   return (
     <Layout title="홈" hasTabBar>
